80abdb8295dc9d6f8d5719b4ad30ac6dd488504d
Leo Rubiano <leo.a.rubiano@gmail.com>
2017-05-25 14:55:43 -0400

    brap

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 //JS and jQuery for RQ
 $('document').ready(function(){
 
+	var crossSelection = document.getElementById('shape-cross');
+
+	var crossSelectionCtx = crossSelection.getContext('2d');
+	crossSelectionCtx.moveTo(0,0);
+	crossSelectionCtx.lineTo(50,50);
+	crossSelectionCtx.stroke();
 
 });
